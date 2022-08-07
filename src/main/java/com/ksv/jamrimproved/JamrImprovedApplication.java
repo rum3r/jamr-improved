d53bee601849bfe04eae7062a1574291b95c27c6
rum3r <kvirk2561@gmail.com>
2022-08-07 23:34:55 +0530

    pushed more files

---
+++ src/main/java/com/ksv/jamrimproved/JamrImprovedApplication.java
@@ -0,0 +1,13 @@
+package com.ksv.jamrimproved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamrImprovedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamrImprovedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksv/jamrimproved/JamrImprovedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksv.jamrimproved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamrImprovedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
