a54e635ec26249832d4669510717e2d5a83ba41b
rum3r <kvirk2561@gmail.com>
2022-08-08 23:58:01 +0530

    Added user and post model

--- src/main/java/com/ksv/jamrimproved/Models/Post.java
+++ src/main/java/com/ksv/jamrimproved/Models/Post.java
@@ -1,4 +1,22 @@
 package com.ksv.jamrimproved.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Data
+@Table(name = "POST_INFO", uniqueConstraints={@UniqueConstraint(columnNames={"id"})})
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+    @Id
+    private String id;
+    private String title;
+    private String body;
 }
