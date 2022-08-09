267359d1c17901e32dcdfa3ace770c821b8a3a2a
rum3r <kvirk2561@gmail.com>
2022-08-09 23:22:10 +0530

    Updated comment model with new columns

--- src/main/java/com/ksv/jamrimproved/Models/Comment.java
+++ src/main/java/com/ksv/jamrimproved/Models/Comment.java
@@ -1,4 +1,41 @@
 package com.ksv.jamrimproved.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "COMMENT_INFO")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private UUID userId;
+
+    private UUID postId;
+
+    private String body;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp updateTime;
+
+    public Comment(UUID userId, UUID postId, String body) {
+        this.userId = userId;
+        this.postId = postId;
+        this.body = body;
+    }
 }
