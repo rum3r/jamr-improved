3e0fbd5d75505ab3ad23fdc199efc86e02e88c22
rum3r <kvirk2561@gmail.com>
2022-08-09 23:06:32 +0530

    Updated post model with new columns

--- src/main/java/com/ksv/jamrimproved/Models/Post.java
+++ src/main/java/com/ksv/jamrimproved/Models/Post.java
@@ -3,20 +3,35 @@ package com.ksv.jamrimproved.Models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @Entity
 @Data
-@Table(name = "POST_INFO", uniqueConstraints={@UniqueConstraint(columnNames={"id"})})
+@Table(name = "POST_INFO")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private UUID userId;
     private String title;
     private String body;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp updateTime;
+    public Post(String title, String body, UUID userId) {
+        this.title = title;
+        this.body = body;
+        this.userId = userId;
+    }
 }
