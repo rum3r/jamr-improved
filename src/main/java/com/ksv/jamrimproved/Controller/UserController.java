ac7f4ef0d45ac53b69dffa60b64752c8c46351d6
rum3r <kvirk2561@gmail.com>
2022-08-11 23:17:34 +0530

    Added user controller

--- src/main/java/com/ksv/jamrimproved/Controller/UserController.java
+++ src/main/java/com/ksv/jamrimproved/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.ksv.jamrimproved.Controller;
 
 import com.ksv.jamrimproved.Models.User;
+import com.ksv.jamrimproved.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +10,13 @@ import java.util.List;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/test")
     public String test() { return "working"; }
 
     @GetMapping("/getAllUsers")
     public List<User> getAllUsers() {
-
+        return userService.getAllUsers();
     }
 }
