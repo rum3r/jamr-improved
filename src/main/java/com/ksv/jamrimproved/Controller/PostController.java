f186b43bad8a2346bcd1bd40d76165efb3a845d4
rum3r <kvirk2561@gmail.com>
2022-08-14 23:36:05 +0530

    Added post controller file

---
+++ src/main/java/com/ksv/jamrimproved/Controller/PostController.java
@@ -0,0 +1,9 @@
+package com.ksv.jamrimproved.Controller;
+
+import com.ksv.jamrimproved.Service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PostController {
+    @Autowired
+    private PostService postService;
+}
---
+++ src/main/java/com/ksv/jamrimproved/Service/PostService.java
@@ -0,0 +1,10 @@
+package com.ksv.jamrimproved.Service;
+
+import com.ksv.jamrimproved.Repo.PostRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PostService {
+    @Autowired
+    private PostRepo repo;
+
+}
