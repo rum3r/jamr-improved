84b6257b93cc9f57cb9070e2e06aab1ce64fdb78
rum3r <kvirk2561@gmail.com>
2022-08-17 22:32:29 +0530

    Added delete post controller

--- src/main/java/com/ksv/jamrimproved/Controller/PostController.java
+++ src/main/java/com/ksv/jamrimproved/Controller/PostController.java
@@ -3,9 +3,12 @@ package com.ksv.jamrimproved.Controller;
 import com.ksv.jamrimproved.Models.Post;
 import com.ksv.jamrimproved.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.UUID;
 
 public class PostController {
     @Autowired
@@ -19,5 +22,10 @@ public class PostController {
     @PutMapping("/editPost")
     public Post editPost(@RequestBody Post post) {
         return postService.addPost(post);
+    }
+
+    @DeleteMapping("deletePost")
+    public void deletePost(@RequestBody(required = true) UUID postId) {
+        postService.deletePost(postId);
     }
 }
--- src/main/java/com/ksv/jamrimproved/Service/PostService.java
+++ src/main/java/com/ksv/jamrimproved/Service/PostService.java
@@ -4,11 +4,19 @@ import com.ksv.jamrimproved.Models.Post;
 import com.ksv.jamrimproved.Repo.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class PostService {
     @Autowired
     private PostRepo repo;
 
     public Post addPost(Post post) {
         return repo.save(post);
+    }
+
+    public void deletePost(UUID postId) {
+        Optional<Post> post = repo.findById(postId);
+        post.ifPresent(value -> repo.delete(value));
     }
 }
